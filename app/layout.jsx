4bb42a3709e4662c86f101dfcbfba2314e97d37b
ALLBLACKHIM <shinyalaluispeter@gmail.com>
2024-11-28 22:11:42 +0200

    the authentication is ongoing

---
+++ app/layout.jsx
@@ -0,0 +1,32 @@
+import '@styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+export const metadata = {
+    title: "Promptopia",
+    description: "A platform for generating and sharing prompts."
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+    <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Promptopia</title>
+    </head>
+    <body>
+        <div className='main'>
+            <div className='gradient' />
+        </div>
+
+        <main className='app'>
+            <Nav />
+            {children}
+        </main>
+    </body>
+    </html>
+  )
+}
+
+export default RootLayout;
